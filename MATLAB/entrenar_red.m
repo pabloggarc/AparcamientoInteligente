d49775e135d51ccb0d77bf96be5d5e6c75f290be
Pablo García <78372183+pabloggarc@users.noreply.github.com>
2024-01-08 15:27:57 +0100

    Más pruebas con la red e inicio memoria

--- MATLAB/entrenar_red.m
+++ MATLAB/entrenar_red.m
@@ -2,15 +2,15 @@
 load("datos.mat"); 
 
 % Elección del número de neuronas de la capa oculta
-neuronas = floor(size(X, 1) * 0.15 * 0.75); 
+neuronas = floor(size(X, 1) * 0.15 * 0.5); 
 
 % Creación de la red
 net = feedforwardnet(neuronas);
 net = configure(net, X', Y');
 
 % Entrenamiento
-net = train(net, X', Y');
+[net, tr] = train(net, X', Y');
 
 % Guardar en local y Simulink
-save("red.mat", "net"); 
+save("red.mat", "net", "tr"); 
 gensim(net); 
